aeeddf6b46b35ffc10c783873cd0c3a8d2598b6a
Gianlu1259 <gianluu68@gmail.com>
2022-01-06 16:12:44 -0300

    commit

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,18 +12,5 @@ api.post('/register',UserController.save_user);
 api.get('/get_user',UserController.get_user);
 api.put('/update_user/:id',UserController.update_user);
 api.delete('/delete_users',UserController.delete_users)
-const AuthController = require('../controllers/Auth')
-
-var api=express.Router();
-
-api.post('/registerUser', AuthController.register)
-api.post('/loginUser', AuthController.login)
-
-
-api.post('/register',UserController.save_user);
-api.get('/get_user',UserController.get_user);
-api.put('/update_user/:id',UserController.update_user);
-api.delete('/delete_users',UserController.delete_users);
-api.delete('/delete_user',UserController.delete_user);
 
 module.exports=api;
